736d3834cb233cd14593f96d9b3d69349533f0dc
bylee <bylee@localhost.localdomain>
2019-05-14 01:03:47 +0900

    Add quicksort3

--- src/main/java/sort/Qsort3.java
+++ src/main/java/sort/Qsort3.java
@@ -1,4 +1,36 @@
 package sort;
 
 public class Qsort3 {
+    private int[] x;
+    public Qsort3(int[] x){
+        this.x = x;
+    }
+
+    public void swap(int l, int u){
+        int tmp = x[l];
+        x[l] = x[u];
+        x[u] = tmp;
+    }
+
+    public void qsort3(int l, int u){
+        if(l >= u)
+            return;
+        int t = x[l];
+        int i = l;
+        int j = u + 1;
+
+        while(true){
+            do i++; while(i <= u && x[i] < t);
+            do j--; while(x[j] > t);
+            if(i > j)
+                break;
+            swap(i, j);
+        }
+        swap(l, j);
+        qsort3(l, j-1);
+        qsort3(j + 1, u);
+    }
+    public int[] getQsort() {
+        return x;
+    }
 }
--- src/test/java/sort/Qsort3Test.java
+++ src/test/java/sort/Qsort3Test.java
@@ -1,4 +1,18 @@
 package sort;
 
-public class Qsort3Test {
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class Qsort3Test extends TestCase {
+    @Test
+    public void testQsort3() {
+        int[] x = {55, 41, 59, 26, 53, 58, 97, 93};
+        int[] res = {26, 41, 53, 55, 58, 59, 93, 97};
+        Qsort3 qs = new Qsort3(x);
+        qs.qsort3(0, 7);
+        int[] q = qs.getQsort();
+        for (int i = 0; i < q.length; i++) {
+            assertEquals(q[i], res[i]);
+        }
+    }
 }
