b476ddd3a436710890b05cb28c5bcfac52c027d9
Beomyong Lee <bylee@bitnine.net>
2020-01-14 08:15:14 +0900

    Add 승지니어/day19 BinaryTreeZigzagLevelOrderTraversal

---
+++ src/test/java/승지니어/day19/BinaryTreeZigzagLevelOrderTraversalTest.java
@@ -0,0 +1,27 @@
+package 승지니어.day19;
+
+import junit.framework.TestCase;
+
+public class BinaryTreeZigzagLevelOrderTraversalTest extends TestCase {
+    public void testBinaryTreeZigzagLevelOrderTraversal() {
+        TreeNode tn3 = new TreeNode(3);
+        TreeNode tn9 = new TreeNode(9);
+        TreeNode tn20 = new TreeNode(20);
+        TreeNode tn15 = new TreeNode(15);
+        TreeNode tn7  = new TreeNode(7);
+
+        tn3.left = tn9;
+        tn3.right = tn20;
+        tn9.left = null;
+        tn9.right = null;
+        tn20.left = tn15;
+        tn20.right = tn7;
+        tn15.left = null;
+        tn15.right = null;
+        tn7.left = null;
+        tn7.right = null;
+
+        BinaryTreeZigzagLevelOrderTraversal btzlot = new BinaryTreeZigzagLevelOrderTraversal();
+        System.out.println(btzlot.zigzagLevelOrder(tn3));
+    }
+}
