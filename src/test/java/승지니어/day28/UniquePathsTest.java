6e23b2bb7f941f4a4483b2fd55322790f5a6aabd
Beomyong Lee <bylee@bitnine.net>
2020-01-23 08:00:57 +0900

    Add 승지니어/day28 UniquePaths

---
+++ src/test/java/승지니어/day28/UniquePathsTest.java
@@ -0,0 +1,12 @@
+package 승지니어.day28;
+
+import junit.framework.TestCase;
+
+public class UniquePathsTest extends TestCase {
+    public void testUniquePaths1() {
+        System.out.println(new UniquePaths().uniquePaths(3, 2));//3
+    }
+    public void testUniquePaths2() {
+        System.out.println(new UniquePaths().uniquePaths(7, 3));//28
+    }
+}
