1db5bb88fcc450c8b60257a649e306b108fa542b
Beomyong Lee <bylee@bitnine.net>
2020-02-06 07:49:45 +0900

    Add 승지니어/day42 GenerateParentheses
    * 문제 끝

---
+++ src/test/java/승지니어/day42/GenerateParenthesesTest.java
@@ -0,0 +1,11 @@
+package 승지니어.day42;
+
+import junit.framework.TestCase;
+
+public class GenerateParenthesesTest extends TestCase {
+    public void testGenerateParentheses() {
+        GenerateParentheses g = new GenerateParentheses();
+        System.out.println(g.generateParenthesis(3));
+    }
+
+}
